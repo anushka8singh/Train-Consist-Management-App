5ecbed29c7d9ba6d83bfdbc99147af6d005bfc04
anushka8singh <anushka0506singh@gmail.com>
2026-04-20 13:28:50 +0530

    anushka added UC7

---
+++ App/UC7.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+class Bogie {
+    String name;
+    int capacity;
+
+    Bogie(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+}
+
+public class UC7 {
+    public static void main(String[] args) {
+        List<Bogie> bogies = new ArrayList<>();
+
+        bogies.add(new Bogie("Sleeper", 72));
+        bogies.add(new Bogie("AC Chair", 56));
+        bogies.add(new Bogie("First Class", 24));
+
+        bogies.sort(Comparator.comparingInt(b -> b.capacity));
+
+        for (Bogie b : bogies) {
+            System.out.println("Bogie: " + b.name + " | Capacity: " + b.capacity);
+        }
+    }
+}
